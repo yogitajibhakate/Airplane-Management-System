b22c40d3031c1df9804212a85a51387aa787c875
yogita <yogita22@navgurukul.org>
2024-12-06 23:10:49 +0530

    Initial commit for Airplane_Mode

--- airplane_mode/airplane_mode/doctype/rent_payment_airport/test_rent_payment_airport.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Yogita and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestRentPaymentairport(FrappeTestCase):
-	pass
